077ddbb8d7adb40817737cd4424415907178b00f
Abhay98-dev <chacharabhay@gmail.com>
2025-11-25 08:43:34 +0530

    "completed"

--- app/(root)/index.jsx
+++ app/(root)/index.jsx
@@ -1,34 +1,87 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
 import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { FlatList,RefreshControl,Text, TouchableOpacity, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
-import { useTransactions } from '../hooks/useTransactions'
+import { useTransactions } from '@/hooks/useTransactions'
+import React, { useEffect , useState} from 'react'
+import PageLoader from '@/components/PageLoader'
+import {styles} from '@/assets/styles/home.styles'
+import { Image } from "react-native"
+import {Ionicons} from '@expo/vector-icons'
+import { useRouter } from "expo-router";
+import { BalanceCard } from '../../components/BalanceCard'
+import { TransactionItem } from '../../components/transactionItem'
+import { Alert } from "react-native";
+import { NoTransactionsFound } from '../../components/NoTransactionsFound'
+
 
 export default function Page() {
   const { user } = useUser()
+  const router=useRouter()
+  const [refreshing,setRefreshing]=useState(false)
+
   const{transactions,summary,isLoading,loadData,deleteTransaction}=useTransactions(user.id)
+
+  const onRefresh= async()=>{
+    setRefreshing(true)
+    await loadData()
+    setRefreshing(false)
+  }
 
   useEffect(()=>{
     loadData()
   },[loadData])
 
-  console.log("Transactions: ",transactions)
-  console.log("Summary: ",summary)
-
+  const handleDelete=(id)=>{
+    Alert.alert("Delete Transaction","are you sure you want to delete this transaction?",[
+      {text:"Cancel",style:"cancel"},
+      {text:"Delete",style:"destructive",onPress:()=>deleteTransaction(id)}
+    ])
+  }
+  if(isLoading && !refreshing){ return <PageLoader/>}
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          <View style={styles.headerLeft}>
+            <Image 
+            source={require("../../assets/images/logo.png")}
+            style={styles.headerLogo}
+            resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.headerRight}>
+            <TouchableOpacity style={styles.addButton} onPress={()=> router.push("/create")}>
+              <Ionicons name="add" size={20} color='#FFF'/>
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton/>
+          </View>
+        </View>
+        <BalanceCard summary={summary}/>
+
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+      </View>
+
+      <FlatList 
+      style={styles.transactionsList}
+      contentContainerStyle={styles.transactionsListContent}
+      data={transactions}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({ item }) => (
+      <TransactionItem item={item} onDelete={handleDelete} />
+    )}
+    ListEmptyComponent={<NoTransactionsFound />}
+    refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
+    />
     </View>
   )
 }
--- hooks/useTransactions.js
+++ hooks/useTransactions.js
@@ -1,63 +1,71 @@
-import { useState } from 'react'
+import { useState , useCallback } from 'react'
 import { Alert } from 'react-native'
 
-const API_URL = "https://localhost:5001/api"
+const API_URL = "https://backend-wallet-18tf.onrender.com/api"
 
 export const useTransactions = (userId) => {
-    const [transactions, setTransactions] = useState([]);
-    const [summary, setSummary]=useState({
-        balance:0,
-        income:0,
-        expenses:0
-    })
-    const [isLoading, setIsLoading] = useState(true);
+  const [transactions, setTransactions] = useState([]);
+  const [summary, setSummary] = useState({
+    balance: 0,
+    income: 0,
+    expenses: 0
+  });
+  const [isLoading, setIsLoading] = useState(true);
 
-    const fetchTransactions = useCallback(async()=>{
-        try{
-            const response=await fetch(`${API_URL}/transactions/${userId}`)
-            const data=await response.json()
-            setTransactions(data)
-        }catch(err){
-            console.error("Error fetching transactions: ",err)
-        }
-    },[userId])
+  const fetchTransactions = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${userId}`);
+      const data = await response.json();
 
-        const fetchSummary = useCallback(async()=>{
-        try{
-            const response=await fetch(`${API_URL}/transactions/summary/${userId}`)
-            const data=await response.json()
-            setSummary(data)
-        }catch(err){
-            console.error("Error fetching Summary: ",err)
-        }
-    },[userId])
+      // Clean and convert data
+      const cleaned = data.map(t => ({
+        ...t,
+        amount: Number(t.amount),            // Fix NaN
+        created_at: new Date(t.created_at),  // Fix Invalid Date
+      }));
 
-    const loadData = useCallback(async()=>{
-       if (!userId) return;
-
-       setIsLoading(true);
-       try{
-        await Promise.all([fetchTransactions(),fetchSummary()])
-       }catch(err){
-        console.error("Error loading data: ",err)
-       }finally{
-        setIsLoading(false)
-       }
-    },[fetchTransactions,fetchSummary,userId])
-
-    const deleteTransaction = async(id)=>{
-        try{
-            const response=await fetch(`${API_URL}/transactions/${id}`,{method:'DELETE'})
-            if(!response.ok){
-                throw new Error("Failed to delete transactions")
-            }
-            loadData()
-            Alert.alert("Success","Transaction deleted successfully")
-        }catch(err){
-            console.error("Error deleting transaction: ",err)
-            Alert.alert("Error",error.message)
-        }
+      setTransactions(cleaned);
+    } catch (err) {
+      console.error("Error fetching transactions: ", err);
     }
+  }, [userId]);
 
-    return{ transactions,summary,isLoading,loadData,deleteTransaction}
-}
+  const fetchSummary = useCallback(async () => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/summary/${userId}`);
+      const data = await response.json();
+      setSummary(data);
+    } catch (err) {
+      console.error("Error fetching Summary: ", err);
+    }
+  }, [userId]);
+
+  const loadData = useCallback(async () => {
+    if (!userId) return;
+
+    setIsLoading(true);
+    try {
+      await Promise.all([fetchTransactions(), fetchSummary()]);
+    } catch (err) {
+      console.error("Error loading data: ", err);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [fetchTransactions, fetchSummary, userId]);
+
+  const deleteTransaction = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}/transactions/${id}`, { method: 'DELETE' });
+      if (!response.ok) {
+        throw new Error("Failed to delete transactions");
+      }
+      loadData();
+      Alert.alert("Success", "Transaction deleted successfully");
+    } catch (err) {
+      console.error("Error deleting transaction: ", err);
+      Alert.alert("Error", err.message);
+    }
+  };
+
+  return { transactions, summary, isLoading, loadData, deleteTransaction };
+};
